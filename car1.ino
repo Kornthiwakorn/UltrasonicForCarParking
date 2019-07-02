21b847f8b05ce2ea1b1fbae8aea247541a4d764b
Kornthiwakorn <52466514+Kornthiwakorn@users.noreply.github.com>
2019-07-03 01:32:32 +0700

    Add files via upload

---
+++ car1.ino
@@ -0,0 +1,98 @@
+#include <HCSR04.h>
+#include <ESP8266WiFi.h> 
+#include  <FirebaseArduino.h>
+
+#define FIREBASE_HOST "carparkingbyiot.firebaseio.com"  
+#define FIREBASE_AUTH "C6ph0A2ebF6lVqTXSpWDK2LQTBdT7RHuvRuEFrM9"  
+
+const char*WIFI_SSID = "iphone11" ;  // ชื่อ WiFi
+const char*WIFI_PASSWORD = "korn1137" ; // รหัส WiFi
+
+HCSR04 hc(D1,D2);
+HCSR04 hc2(D3,D6);
+
+
+
+
+void setup() {
+  
+  
+  Serial.begin(115200);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+  
+    Serial.print(".");
+    delay(500);
+   
+  }
+ 
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+ 
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+ 
+ 
+}
+
+void loop() {
+ 
+  double a1 = hc.dist();
+  double a2 = hc2.dist();
+  Serial.print( "a1 = ");
+  Serial.print( hc.dist() ); Serial.println("   CM");
+   Serial.print( "a2 = ");
+  Serial.print( hc2.dist()); Serial.println("   CM"); 
+   delay(500);
+  
+  
+  if(a1<10){
+  
+  Firebase.setInt("/ParkingInfo/-LdULjKjdsI17-T-LLMp/a1Status",1 );
+
+  
+  
+  }else{
+    
+    Firebase.setInt("/ParkingInfo/-LdULjKjdsI17-T-LLMp/a1Status",0);
+  
+  }
+
+  if(a2<10){
+      
+      Firebase.setInt("/ParkingInfo/-LdULjKjdsI17-T-LLMp/a2Status",1 );
+      
+    }else{
+      
+    
+    Firebase.setInt("/ParkingInfo/-LdULjKjdsI17-T-LLMp/a2Status",0);
+  }
+
+  
+   int parkoneSlot = Firebase.getInt("/ParkingInfo/-LdULjKjdsI17-T-LLMp/nSlot");
+   int a1Status = Firebase.getInt("/ParkingInfo/-LdULjKjdsI17-T-LLMp/a1Status");
+   int a2Status = Firebase.getInt("/ParkingInfo/-LdULjKjdsI17-T-LLMp/a2Status");
+   
+   int parkOneTotal = parkoneSlot-(a1Status+a2Status);
+   
+   
+
+   if(parkOneTotal>0){
+       Firebase.setInt("/ParkingInfo/-LdULjKjdsI17-T-LLMp/empSlot",parkOneTotal );
+      
+      
+    }else if (parkOneTotal==0){
+     Firebase.setString("/ParkingInfo/-LdULjKjdsI17-T-LLMp/empSlot","เต็ม");
+    }
+   
+   
+   
+   
+  
+    
+  
+
+}
---
+++ car2.ino
@@ -0,0 +1,91 @@
+#include <HCSR04.h>
+#include <ESP8266WiFi.h> 
+#include <FirebaseArduino.h>
+#include <ArduinoJson.h>
+
+#define FIREBASE_HOST "carparkingbyiot.firebaseio.com"  
+#define FIREBASE_AUTH "C6ph0A2ebF6lVqTXSpWDK2LQTBdT7RHuvRuEFrM9"  
+
+const char*WIFI_SSID = "iphone11" ;  // ชื่อ WiFi
+const char*WIFI_PASSWORD = "korn1137" ; // รหัส WiFi
+
+HCSR04 hc(D1,D2);
+HCSR04 hc2(D5,D6);
+
+
+
+
+void setup() {
+ 
+  Serial.begin(115200);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+   
+  }
+ 
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+ 
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+ 
+}
+
+void loop() {
+  double b1 = hc.dist();
+  double b2 = hc2.dist();
+  Serial.print( hc.dist() ); Serial.println("   CM");
+  Serial.print( hc2.dist()); Serial.println("   CM"); 
+  
+  
+  if(b1<10){
+  
+  Firebase.setInt("ParkingTwo/B1_Status", 1 );
+
+  
+  
+  }else{
+    
+    Firebase.setInt("ParkingTwo/B1_Status", 0 );
+  
+  }
+
+  if(b2<10){
+      
+      Firebase.setInt("ParkingTwo/B2_Status", 1 );
+      
+    }else{
+      
+    
+    Firebase.setInt("ParkingTwo/B2_Status", 0 );
+  }
+
+  
+
+   int b1Status = Firebase.getInt("ParkingTwo/B1_Status");
+   int b2Status = Firebase.getInt("ParkingTwo/B2_Status");
+   int parkTwoTotal = 2-(b1Status+b2Status);
+   
+   
+
+   if(parkTwoTotal>0){
+       Firebase.setInt("ParkingInfo/ParkingTwo/SlotTotal",parkTwoTotal );
+      
+      
+    }else if (parkTwoTotal==0){
+     Firebase.setInt("ParkingInfo/ParkingTwo/SlotTotal",0 );
+    }
+   
+   
+   
+   
+   
+    
+  delay(500);
+
+}
